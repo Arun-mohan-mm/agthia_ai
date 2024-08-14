ac98a15f2ac3099f5867d9eb515c69e321cf0a7f
Arun-mohan-mm <arunm8181@gmail.com>
2024-08-14 10:16:37 +0530

    agthiaai commit

---
+++ agthi/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from . models import *
+
+
+admin.site.register(Registration)
+admin.site.register(Job_recruitment)
+admin.site.register(Job_application)
+admin.site.register(Restaurants)
+admin.site.register(Restaurant_images)
+admin.site.register(Subscriptions)
+admin.site.register(Aboutt)
+admin.site.register(Our_people)
+admin.site.register(Mission)
+admin.site.register(Vision)
+admin.site.register(Word_from_CEO)
